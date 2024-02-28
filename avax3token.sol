c6a26574653d7c1e9486c6928424331c4894d24a
Vikash2398 <160607273+Vikash2398@users.noreply.github.com>
2024-02-29 01:05:24 +0530

    Update avax3token.sol

--- avax3token.sol
+++ avax3token.sol
@@ -14,20 +14,14 @@ contract SpiderManToken is ERC20, ERC20Burnable, Ownable {
     }
 
     function mintTokens(address to, uint256 amount) public onlyOwner {
-        require(to != address(0), "Invalid address");
-        require(amount > 0, "Invalid amount");
         _mint(to, amount);
     }
 
     function burnTokens(address from, uint256 amount) public onlyOwner {
-        require(from != address(0), "Invalid address");
-        require(balanceOf(from) >= amount, "Insufficient balance");
         _burn(from, amount);
     }
 
    function transfer(address to, uint256 amount) public override returns (bool) {
-    require(to != address(0), "Invalid recipient address");
-    require(amount > 0, "Invalid transfer amount");
     require(balanceOf(msg.sender) >= amount, "Insufficient balance");
 
     _transfer(msg.sender, to, amount);
